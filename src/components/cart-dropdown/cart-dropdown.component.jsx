80c30c17ac73adfcbfc9a408849d951013cd1e0b
Tito-86 <thomas86@gmail.com>
2023-03-17 20:23:20 +0100

    adding checkout route component checkout have to finish the scss in this component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { useContext } from "react";
+
 import { CartContext } from "../../contexts/cart.context";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+  const navigation = useNavigate();
   const { cartItems } = useContext(CartContext);
+
+  const goToCkechoutHandler = () => {
+    navigation("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -13,7 +22,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECK OUT</Button>
+      <Button onClick={goToCkechoutHandler}>GO TO CHECK OUT</Button>
     </div>
   );
 };
