8bdc36df2b36c0f0ece13bd01ee4524c0e615e23
Tito-86 <thomas86@gmail.com>
2023-05-17 11:08:16 +0200

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import envCompatible from "vite-plugin-env-compatible";
 import svgr from "vite-plugin-svgr";
 export default defineConfig({
+  build: {
+    rollupOptions: { external: [/^node:.*/] },
+  },
   resolve: {
     alias: [
       {
